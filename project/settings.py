9065947eb01120d3cd3f0540a3a25d528acfd50a
Vladimir Leveryev <3712774+volodya-leveryev@users.noreply.github.com>
2022-12-15 17:21:00 +0900

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -116,8 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'ghcr.io/volodya-leveryev/students:latest
-
+STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
